3ad5080721a189615d997efa7df268cd1ec6b983
yanghui <yanghui1@superatomfin.com>
2025-10-17 16:15:09 +0800

    help page

--- app/src/main/java/com/example/tubedown/main/help/HelpFragment.kt
+++ app/src/main/java/com/example/tubedown/main/help/HelpFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.lifecycle.ViewModelProvider
 import com.example.databinding.FragmentHelpBinding
 import com.example.tubedown.main.base.BaseFragment
 import com.example.tubedown.main.home.MainActivity
@@ -13,9 +14,13 @@ import javax.inject.Inject
 class HelpFragment : BaseFragment() {
 
     private lateinit var dataBinding: FragmentHelpBinding
+    private lateinit var helpViewModel: HelpViewModel
 
     @Inject
     lateinit var mainActivity: MainActivity
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     companion object {
         @JvmStatic
@@ -26,13 +31,20 @@ class HelpFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val color = getThemeBackgroundColor()
+        helpViewModel = ViewModelProvider(this, viewModelFactory)[HelpViewModel::class.java]
 
         dataBinding = FragmentHelpBinding.inflate(inflater, container, false).apply {
-            this.getStartedOkButton.setOnClickListener {
+            viewModel = helpViewModel
+            
+            // Setup toolbar
+            toolbar.setNavigationOnClickListener {
                 parentFragmentManager.popBackStack()
             }
-            this.helpContainer.setBackgroundColor(color)
+            
+            // Setup FAB button
+            getStartedOkButton.setOnClickListener {
+                parentFragmentManager.popBackStack()
+            }
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
@@ -42,5 +54,13 @@ class HelpFragment : BaseFragment() {
         return dataBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        helpViewModel.start()
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        helpViewModel.stop()
+    }
 }
